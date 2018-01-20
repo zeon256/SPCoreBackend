a9cfd68c37e239754f01bf0ae12c6be40721ca28
euwbah <euwbah@gmail.com>
2018-01-20 23:33:47 +0800

    Made timetable data mining multi-threaded, haven't tested

--- src/main/kotlin/routes/Event.kt
+++ src/main/kotlin/routes/Event.kt
@@ -1,10 +1,16 @@
 package routes
 
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.gson.responseObject
 import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
 import io.ktor.routing.Route
 import io.ktor.routing.route
 import io.ktor.util.ValuesMap
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 import models.TimeTable
@@ -15,6 +21,9 @@ import java.time.LocalDate
 import java.time.YearMonth
 import java.util.*
 import kotlin.collections.ArrayList
+
+// Prevent quadruple nested generics
+private typealias FuelRRR = Triple<Request, Response, Result<TimetableFromSpice, FuelError>>
 
 fun Route.event(path: String) = route("$path/auth") {
 
@@ -42,25 +51,62 @@ fun getTimeTableFromSpice() {
     val arrListOfLesson = ArrayList<TimeTable.Lesson>()
     var dayNo = 1
 
-    //generates date in ddMMyy since first day of the current month to end of the month
+    val asyncResponses =
+            mutableListOf<Deferred<Pair<FuelRRR, String>>>()
+
+    // generates date in ddMMyy since first day of the current month to end of the month
     do {
-        val temp= LocalDate.now().withDayOfMonth(dayNo).toString()
+        val temp = LocalDate.now().withDayOfMonth(dayNo).toString()
         val original= originalFormat.parse(temp)
         val dateStr = targetDateFormat.format(original)
 
-        //arListOfDates.add(targetDateFormat.format(original))
+        // arListOfDates.add(targetDateFormat.format(original))
         val url = "http://mobileappnew.sp.edu.sg/spTimetable/source/sptt.php?DDMMYY=$dateStr&id=1626175"
 
+        /* Original Non async variation
         val (_, response, res) =
-                url.httpPost().responseObject(TimetableFromSpice.Deserializer())
+               url.httpPost().responseObject(TimetableFromSpice.Deserializer())
+
+
         println(dayNo)
+
         //this has to be reversed because SP server returns the last lesson first
-        res.get().timetable.asReversed().forEach { it.toLesson(dateStr)?.let { it1 -> arrListOfLesson.add(it1) } }
+        res.get().timetable.asReversed()
+                .forEach {
+                    it.toLesson(dateStr)?.let {
+                        it1 ->
+                        arrListOfLesson.add(it1)
+                    }
+                }
+        */
+
+        // Experiemental Async
+        asyncResponses.add(
+                async {
+                    println("Async: $dateStr")
+                    Pair(url.httpPost().responseObject(TimetableFromSpice.Deserializer()),
+                         dateStr)
+                })
 
         dayNo ++
-    }while (dayNo-1 < noOfDaysInMonth)
+    } while (dayNo-1 < noOfDaysInMonth)
+
+    asyncResponses.forEach {
+        val (fuelRRR, dateStr) =
+                runBlocking { it.await() }
+
+        println("Awaited: $dateStr")
+
+        val (_, _, res) = fuelRRR
+
+        res.get().timetable.asReversed()
+                .forEach {
+                    it.toLesson(dateStr)?.apply {
+                        arrListOfLesson.add(this)
+                    }
+                }
+    }
 
     println(arrListOfLesson)
-
 
 }
